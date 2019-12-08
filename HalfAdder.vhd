f423c9e0146e25a10b377138fb9de48f164032fd
Christopher Moroney <christopher.moroney1@gmail.com>
2019-12-08 01:59:20 -0800

    Update and rename halfAdder.vhd to HalfAdder.vhd

--- HalfAdder.vhd
+++ HalfAdder.vhd
@@ -1,11 +1,11 @@
 -- this VHDL code shows how binary numbers would be added together, but only maintaining the same number of bits, hense why
 -- it is only a half adder. (10 + 10 == 00 in half adder)
 
-Entity halfAdder is
+Entity HalfAdder is
   port(X, Y: in bit; Sum, Cout: out bit);
+End Entity HalfAdder;
   
-End Entity halfAdder;
-Architecture behavior OF halfAdder IS
+Architecture behavior OF HalfAdder IS
 Begin
   Sum <= (X xor Y);
   Cout <= (X and Y);
